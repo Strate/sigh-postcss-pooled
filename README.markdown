90144c502a1dbe711c962ae416a804721d2c6c67
Artur Eshenbrener <strate@yandex.ru>
2016-04-13 17:29:04 +0300

    Init

---
+++ README.markdown
@@ -0,0 +1,14 @@
+# [WIP] NOT PRODUCTION READY YET
+
+# sigh-postcss-pooled
+
+[![build status](https://circleci.com/gh/Strate/sigh-postcss-pooled.png)](https://circleci.com/gh/Strate/sigh-postcss-pooled)
+
+Sigh plugin for postcss using sigh process pooling
+
+## Example
+
+`npm install --save-dev sigh-postcss-pooled` then add something like this to your `sigh.js`:
+```javascript
+// TODO: example goes here
+```
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import _ from 'lodash'
+import Promise from 'bluebird'
+import { Bacon } from 'sigh-core'
+import { mapEvents } from 'sigh-core/lib/stream'
+
+function postcssPooledTask(opts) {
+  // this function is called once for each subprocess in order to cache state,
+  // it is not a closure and does not have access to the surrounding state, use
+  // `require` to include any modules you need, for further info see
+  // https://github.com/ohjames/process-pool
+  var log = require('sigh-core').log;
+  var pluginsFactory = require(opts.pluginsFactory);
+  var postcss = require("postcss")(pluginsFactory());
+
+  // this task runs inside the subprocess to transform each event
+  return event => {
+    return postcss.process(event.data, {
+      from: event.path,
+      map: {
+        inline: false
+      }
+    }).then(result => ({
+      data: result.css,
+      sourceMap: result.map
+    }));
+  }
+}
+
+function adaptEvent(compiler) {
+  // data sent to/received from the subprocess has to be serialised/deserialised
+  return event => {
+    if (event.type !== 'add' && event.type !== 'change')
+      return event;
+
+    if (event.fileType !== 'css') return event;
+
+    return compiler(_.pick(event, 'type', 'data', 'path', 'projectPath')).then(result => {
+      event.data = result.data;
+
+      if (result.sourceMap) {
+        event.applySourceMap(result.sourceMap);
+      }
+
+
+      return event
+    })
+  }
+}
+
+var pooledProc;
+
+export default function(op, opts = {}) {
+  if (! pooledProc)
+    pooledProc = op.procPool.prepare(postcssPooledTask, opts, { module });
+
+  return mapEvents(op.stream, adaptEvent(pooledProc))
+}
